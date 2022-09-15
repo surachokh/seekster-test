06b536809fc74808aeaa7184819c3f27776c4b18
Independent <38752974+surachokh@users.noreply.github.com>
2022-09-15 12:06:01 +0700

    Add theme

---
+++ src/common/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  fontSizes: [14, 16, 24, 32, 48],
+  fontWeights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  space: [4, 8, 12, 16, 32, 64, 128],
+
+  colors: {
+    pricetag: `#ffc600`,
+    info: `#027aff`,
+  },
+};
+
+export default theme;
