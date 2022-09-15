0131bbb69d8435d162948f59bc4c203ad6c5d748
Independent <38752974+surachokh@users.noreply.github.com>
2022-09-15 12:05:28 +0700

    Set route, theme and provider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import Home from "./pages/Home";
+import theme from "./common/theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Detail from "./pages/Detail";
+import Menu from "./components/Menu";
+import Order from "./pages/Order";
+import UserProvider from "./Contexts/UserProvider";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <UserProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Menu />}>
+              <Route path="/detail/:id" element={<Detail />} />
+              <Route path="/order" element={<Order />} />
+              <Route index element={<Home />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
+    </ThemeProvider>
   );
 }
-
-export default App;
